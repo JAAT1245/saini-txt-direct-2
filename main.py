93ef8c0b2b031f23b07c442a8749c36fb71538b6
CHENARAMKHICHAR2003 <crchoudharyji1122@gmail.com>
2025-08-31 20:27:43 +0530

    Update main.py

--- main.py
+++ main.py
@@ -504,7 +504,7 @@ async def start(bot, m: Message):
     caption = f"🌟 Welcome {m.from_user.mention} ! 🌟"
     start_message = await bot.send_photo(
         chat_id=m.chat.id,
-        photo="https://envs.sh/GVI.jpg",
+        photo="https://files.catbox.moe/9t90ws.jpg",
         caption=caption
     )
 
@@ -662,7 +662,7 @@ async def help_button(client, callback_query):
     
   await callback_query.message.edit_media(
     InputMediaPhoto(
-      media="https://tinypic.host/images/2025/07/14/file_00000000fc2461fbbdd6bc500cecbff8_conversation_id6874702c-9760-800e-b0bf-8e0bcf8a3833message_id964012ce-7ef5-4ad4-88e0-1c41ed240c03-1-1.jpg",
+      media="https://files.catbox.moe/9t90ws.jpg",
       caption=caption
     ),
     reply_markup=keyboard
